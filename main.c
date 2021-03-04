d75478c6e834722d31347727f2e7c5f66652b062
rosendomanas <71023382+rosendomanas@users.noreply.github.com>
2021-03-04 13:11:29 +0100

    tp2-move

--- main.c
+++ main.c
@@ -2,63 +2,39 @@
 #include <system_clock_config.h>
 #include <gpio.h>
 #include <main.h>
+#include <timer.h>
+#include <motor.h>
+#include <selector.h>
+
+#define PI                  3.1415926536f
+//TO ADJUST IF NECESSARY. NOT ALL THE E-PUCK2 HAVE EXACTLY THE SAME WHEEL DISTANCE
+#define WHEEL_DISTANCE      5.35f    //cm
+#define PERIMETER_EPUCK     (PI * WHEEL_DISTANCE)
 
 // Init function required by __libc_init_array
 void _init(void) {}
+
+// Simple delay function
+void delay(unsigned int n)
+{
+    while (n--) {
+        __asm__ volatile ("nop");
+    }
+}
+
 
 int main(void)
 {
     SystemClock_Config();
 
-    // Enable GPIOD peripheral clock
-    RCC->AHB1ENR    |= RCC_AHB1ENR_GPIODEN;
-    RCC->AHB1ENR    |= RCC_AHB1ENR_GPIOBEN;// @suppress("Field cannot be resolved")
+    gpio_config_output_af_pushpull(GPIOD, 14);
+    timer4_start();
 
-    // LED used init
-    gpio_config_output_opendrain(LED_USED);
-    gpio_clear(LED_USED);
-    gpio_config_output_opendrain(LED1);
-    gpio_clear(LED1);
-    gpio_config_output_opendrain(LED3);
-    gpio_clear(LED3);
-    gpio_config_output_opendrain(LED5);
-    gpio_clear(LED5);
-    gpio_config_output_opendrain(LED7);
-    gpio_clear(LED7);
+    // Enable GPIOD and GPIOE peripheral clock
+    RCC->AHB1ENR    |= RCC_AHB1ENR_GPIOBEN | RCC_AHB1ENR_GPIODEN;
 
-    //gpio_config_input(GPIOD, 4);
-
-
-    timer7_start( );
-    while (1){}
-
-    /*while (1) {
-    	/*gpio_toggle(LED1);
-
-    	for(uint32_t i = 0;i<1000000; ++i){}
-    	gpio_toggle(LED3);
-
-    	for(uint32_t i = 0;i<1000000; ++i){}
-    	gpio_toggle(LED5);
-
-    	for(uint32_t i = 0;i<1000000; ++i){}
-    	gpio_toggle(LED7);
-
-    	gpio_toggle(LED1);
-    	gpio_toggle(LED3);
-    	gpio_toggle(LED5);
-		gpio_toggle(LED7);
-
-    	if (gpio_read_input(GPIOD, 4) == 1)
-    	{
-    		gpio_toggle(LED1);
-    		gpio_toggle(LED3);
-    		gpio_toggle(LED5);
-    		gpio_toggle(LED7);
-    	}
-    	for(uint32_t i = 0;i<1000000; ++i){}
-
-
-
-    }*/
+    while (1) {
+        
+    }
 }
+
