9458aae69dcf64c2c974c8d37e4735b4f5393ab4
rosendomanas <71023382+rosendomanas@users.noreply.github.com>
2021-04-01 12:56:33 +0200

    tp5_v1

--- main.c
+++ main.c
@@ -1,217 +1,143 @@
-#include <ch.h>
-#include <hal.h>
-#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "ch.h"
+#include "hal.h"
+#include "memory_protection.h"
+#include <usbcfg.h>
+#include <main.h>
 #include <chprintf.h>
-#include <messagebus.h>
-#include <i2c_bus.h>
-#include <imu.h>
+#include <motors.h>
+#include <audio/microphone.h>
 
-#define NB_SAMPLES_OFFSET     200
+#include <audio_processing.h>
+#include <fft.h>
+#include <communications.h>
+#include <arm_math.h>
 
-messagebus_t bus;
-MUTEX_DECL(bus_lock);
-CONDVAR_DECL(bus_condvar);
+//uncomment to send the FFTs results from the real microphones
+#define SEND_FROM_MIC
 
-void panic_handler(const char *reason)
-{
-    (void)reason;
-
-    palClearPad(GPIOD, GPIOD_LED1);
-    palClearPad(GPIOD, GPIOD_LED3);
-    palClearPad(GPIOD, GPIOD_LED5);
-    palClearPad(GPIOD, GPIOD_LED7);
-    palClearPad(GPIOD, GPIOD_LED_FRONT);
-    palClearPad(GPIOB, GPIOB_LED_BODY);
-    
-    while (true) {
-
-    }
-}
+//uncomment to use double buffering to send the FFT to the computer
+//#define DOUBLE_BUFFERING
 
 static void serial_start(void)
 {
-    static SerialConfig ser_cfg = {
-        115200,
-        0,
-        0,
-        0,
-    };
+	static SerialConfig ser_cfg = {
+	    115200,
+	    0,
+	    0,
+	    0,
+	};
 
-    sdStart(&SD3, &ser_cfg); // UART3. Connected to the second com port of the programmer
+	sdStart(&SD3, &ser_cfg); // UART3.
 }
 
-static void timer11_start(void){
+static void timer12_start(void){
     //General Purpose Timer configuration   
-    //timer 11 is a 16 bit timer so we can measure time
+    //timer 12 is a 16 bit timer so we can measure time
     //to about 65ms with a 1Mhz counter
-    static const GPTConfig gpt11cfg = {
+    static const GPTConfig gpt12cfg = {
         1000000,        /* 1MHz timer clock in order to measure uS.*/
         NULL,           /* Timer callback.*/
         0,
         0
     };
 
-    gptStart(&GPTD11, &gpt11cfg);
+    gptStart(&GPTD12, &gpt12cfg);
     //let the timer count to max value
-    gptStartContinuous(&GPTD11, 0xFFFF);
-}
-
-static THD_WORKING_AREA(waThdFrontLed, 128);
-static THD_FUNCTION(ThdFrontLed, arg) {
-
-    chRegSetThreadName(__FUNCTION__);
-    (void)arg;
-
-    systime_t time;
-
-    while(1){
-        time = chVTGetSystemTime();
-        palTogglePad(GPIOD, GPIOB_LED_BODY);
-        chThdSleepUntilWindowed(time, time + /*MS2ST(500)*/ 500);
-    }
-}
-
-static THD_WORKING_AREA(waThdBodyLed, 128);
-static THD_FUNCTION(ThdBodyLed, arg) {
-
-    chRegSetThreadName(__FUNCTION__);
-    (void)arg;
-
-    while(1){
-        palTogglePad(GPIOB, GPIOB_LED_BODY);
-
-        /*
-        *   1st case :  pause the thread during 500ms
-        */
-        chThdSleepMilliseconds(500);
-
-        /*
-        *   2nd case :  make the thread work during the 500ms
-        */
-
-        // //about 500ms at 168MHz
-        /*for(uint32_t i = 0 ; i < 21000000 ; i++){
-             __asm__ volatile ("nop");
-        }*/
-
-        /*
-        *   3rd case :  make the thread work during the 500ms
-        *               and block the preemption
-        */
-
-        /*chSysLock();
-        for(uint32_t i = 0 ; i < 21000000 ; i++){
-            __asm__ volatile ("nop");
-        }
-        chSysUnlock();*/
-    }
-}
-
-void show_gravity(imu_msg_t *imu_values){
-
-    //variable to measure the time some functions take
-    //volatile to not be optimized out by the compiler if not used
-    volatile uint16_t time = 0;
-
-    /*
-    *   Use this to reset the timer counter and prevent the system
-    *   to switch to another thread.
-    *   Place it at the beginning of the code you want to measure
-    */
-    chSysLock();
-    //reset the timer counter
-    GPTD11.tim->CNT = 0;
-
-    /*
-    *   Use this to capture the counter and stop to prevent 
-    *   the system to switch to another thread.
-    *   Place it at the end of the code you want to measure
-    */
-    time = GPTD11.tim->CNT;
-    chSysUnlock();
-
-    /*
-    *   TASK 11 : TO COMPLETE
-    */
-    if ( imu_values->acceleration[X_AXIS]>3 ){
-    	palClearPad(GPIOD, GPIOD_LED7); //led7 port d
-    }else {
-    	palSetPad(GPIOD, GPIOD_LED7);
-    }
-    if ( imu_values->acceleration[X_AXIS]<-3 ){
-       	palClearPad(GPIOD, GPIOD_LED3);
-    }else {
-    	palSetPad(GPIOD, GPIOD_LED3);
-    }
-    if ( imu_values->acceleration[Y_AXIS]>3 ){
-		palClearPad(GPIOD, GPIOD_LED5);
-	}else {
-    	palSetPad(GPIOD, GPIOD_LED5);
-    }
-    if ( imu_values->acceleration[Y_AXIS]<-3 ){
-		palClearPad(GPIOD, GPIOD_LED1);
-	}else {
-    	palSetPad(GPIOD, GPIOD_LED1);
-    }
-
-
+    gptStartContinuous(&GPTD12, 0xFFFF);
 }
 
 int main(void)
 {
-    /* System init */
+
     halInit();
     chSysInit();
+    mpu_init();
+
+    //starts the serial communication
     serial_start();
-    timer11_start();
-    i2c_start();
-    imu_start();
+    //starts the USB communication
+    usb_start();
+    //starts timer 12
+    timer12_start();
+    //inits the motors
+    motors_init();
 
-    /** Inits the Inter Process Communication bus. */
-    messagebus_init(&bus, &bus_lock, &bus_condvar);
+    //send_tab is used to save the state of the buffer to send (double buffering)
+    //to avoid modifications of the buffer while sending it
+    static float send_tab[FFT_SIZE];
+
+#ifdef SEND_FROM_MIC
+    //starts the microphones processing thread.
+    //it calls the callback given in parameter when samples are ready
+    mic_start(&processAudioData);
+#endif  /* SEND_FROM_MIC */
+
+    /* Infinite loop. */
+    volatile uint32_t time = 0;
+
+    while (1) {
+#ifdef SEND_FROM_MIC
+        //waits until a result must be sent to the computer
+        wait_send_to_computer();
+#ifdef DOUBLE_BUFFERING
+        //we copy the buffer to avoid conflicts
+        arm_copy_f32(get_audio_buffer_ptr(LEFT_OUTPUT), send_tab, FFT_SIZE);
+        SendFloatToComputer((BaseSequentialStream *) &SD3, send_tab, FFT_SIZE);
+#else
+        SendFloatToComputer((BaseSequentialStream *) &SD3, get_audio_buffer_ptr(LEFT_OUTPUT), FFT_SIZE);
+#endif  /* DOUBLE_BUFFERING */
+#else
 
 
-    /*
-    *   TASKS 3,4,5,6,7 : UNDERSTANDING THREADS ON CHIBIOS
-    */
-    chThdCreateStatic(waThdFrontLed, sizeof(waThdFrontLed), NORMALPRIO, ThdFrontLed, NULL);
-    chThdCreateStatic(waThdBodyLed, sizeof(waThdBodyLed), NORMALPRIO, ThdBodyLed, NULL);
+        float* bufferCmplxInput = get_audio_buffer_ptr(LEFT_CMPLX_INPUT);
+        float* bufferOutput = get_audio_buffer_ptr(LEFT_OUTPUT);
+        static complex_float CmplxbufferCmplxInput [FFT_SIZE];
 
-    //to change the priority of the thread invoking the function. The main function in this case
-    //chThdSetPriority(NORMALPRIO+2);
+        uint16_t size = ReceiveInt16FromComputer((BaseSequentialStream *) &SD3, bufferCmplxInput, FFT_SIZE);
 
-    messagebus_topic_t *imu_topic = messagebus_find_topic_blocking(&bus, "/imu");
-    imu_msg_t imu_values;
+        for (uint16_t i=0; i<FFT_SIZE; ++i){
+        	CmplxbufferCmplxInput[i].real=bufferCmplxInput[2*i];
+        	CmplxbufferCmplxInput[i].imag=bufferCmplxInput[2*i+1];
 
-    //wait 2 sec to be sure the e-puck is in a stable position
-    chThdSleepMilliseconds(2000);
-    imu_compute_offset(imu_topic, NB_SAMPLES_OFFSET);
+        	//CmplxbufferCmplxInput[i].real=bufferCmplxInput[2*i];
+        	//CmplxbufferCmplxInput[i].imag=bufferCmplxInput[2*i+1];
+        }
 
-    while(1){
-        //wait for new measures to be published
-        messagebus_topic_wait(imu_topic, &imu_values, sizeof(imu_values));
-        //prints raw values
-        chprintf((BaseSequentialStream *)&SD3, "%Ax=%-7d Ay=%-7d Az=%-7d Gx=%-7d Gy=%-7d Gz=%-7d\r\n", 
-                imu_values.acc_raw[X_AXIS], imu_values.acc_raw[Y_AXIS], imu_values.acc_raw[Z_AXIS], 
-                imu_values.gyro_raw[X_AXIS], imu_values.gyro_raw[Y_AXIS], imu_values.gyro_raw[Z_AXIS]);
+        if(size == FFT_SIZE){
 
-        //prints raw values with offset correction
-        chprintf((BaseSequentialStream *)&SD3, "%Ax=%-7d Ay=%-7d Az=%-7d Gx=%-7d Gy=%-7d Gz=%-7d\r\n", 
-                imu_values.acc_raw[X_AXIS]-imu_values.acc_offset[X_AXIS], 
-                imu_values.acc_raw[Y_AXIS]-imu_values.acc_offset[Y_AXIS], 
-                imu_values.acc_raw[Z_AXIS]-imu_values.acc_offset[Z_AXIS], 
-                imu_values.gyro_raw[X_AXIS]-imu_values.gyro_offset[X_AXIS], 
-                imu_values.gyro_raw[Y_AXIS]-imu_values.gyro_offset[Y_AXIS], 
-                imu_values.gyro_raw[Z_AXIS]-imu_values.gyro_offset[Z_AXIS]);
+            //doFFT_optimized(FFT_SIZE, bufferCmplxInput);
+        	chSysLock();
+        	//reset the timer counter
+        	GPTD12.tim->CNT = 0;
 
-        //prints values in readable units
-        chprintf((BaseSequentialStream *)&SD3, "%Ax=%.2f Ay=%.2f Az=%.2f Gx=%.2f Gy=%.2f Gz=%.2f (%x)\r\n\n", 
-                imu_values.acceleration[X_AXIS], imu_values.acceleration[Y_AXIS], imu_values.acceleration[Z_AXIS], 
-                imu_values.gyro_rate[X_AXIS], imu_values.gyro_rate[Y_AXIS], imu_values.gyro_rate[Z_AXIS], 
-                imu_values.status);
-       
-        show_gravity(&imu_values);
-        chThdSleepMilliseconds(100);
+            doFFT_optimized(FFT_SIZE, bufferCmplxInput);
+
+        	//doFFT_c(FFT_SIZE, CmplxbufferCmplxInput);
+        	/*for(uint16_t i=0;i<2*FFT_SIZE;i=i+2){
+        		bufferCmplxInput[i] = CmplxbufferCmplxInput[i/2].real;
+        		bufferCmplxInput[i+1] = CmplxbufferCmplxInput[i/2].imag;
+        	}*/
+
+            arm_cmplx_mag_f32(bufferCmplxInput, bufferOutput, FFT_SIZE);
+            time = GPTD12.tim->CNT;
+            chSysUnlock();
+            chprintf((BaseSequentialStream *)&SDU1, "time=%dus\n", time);
+
+            SendFloatToComputer((BaseSequentialStream *) &SD3, bufferOutput, FFT_SIZE);
+
+        }
+#endif  /* SEND_FROM_MIC */
     }
+}
 
+#define STACK_CHK_GUARD 0xe2dee396
+uintptr_t __stack_chk_guard = STACK_CHK_GUARD;
+
+void __stack_chk_fail(void)
+{
+    chSysHalt("Stack smashing detected");
 }
