c2cc34f371aebd514798a6199f5bd0b96a4d6a22
rosendomanas <71023382+rosendomanas@users.noreply.github.com>
2021-05-13 10:19:39 +0200

    Update position_calibrator.c
    final code with  some warnings

--- MiniProject/position_calibrator.c
+++ MiniProject/position_calibrator.c
@@ -1,40 +1,51 @@
 /*
  * position_calibrator.c
- *
+ * modified VL53L0X.c
  *  Created on: May 8, 2021
  *      Author: rosendo
  */
 #include <position_calibrator.h>
 #include "ch.h"
 #include "hal.h"
-#include "VL53L0X.h"
-#include "Api/core/inc/vl53l0x_api.h"
+#include "sensors/VL53L0X/VL53L0X.h"
+#include "sensors/VL53L0X/Api/core/inc/vl53l0x_api.h"
 #include "shell.h"
 #include "chprintf.h"
 #include "i2c_bus.h"
 #include "usbcfg.h"
+#include "motors_controller.h"
 #include "motors.h"
 
+#define CORNER_DISTANCE 150 //uncalibrated distance in mm to each wall
+#define AVG_MOV_K 0.01
+#define MINIMAL_DIST 100000.0 //a minimal distance big enough needs to be set as initialization
+#define WALL_X_ANGLE 270
+#define WALL_Y_ANGLE 180
+#define CALIBRATION_ANGLE 160 //the robot turns almost to the Y wall and starts scanning
+#define CALIBRATION_SPEED 20 //the speed of the robot while scanning the wall
+#define THREAD_SLEEP_YES 5 //if a calibration is done, then 5 ms is a good value(found by trial and error)
+#define THREAD_SLEEP_NO 500
+
+enum CalibState{POSITIONING, TURNING_TO_WALL_Y, CALIBRATING_PHASE_1, TURNING_TO_WALL_X, CALIBRATING_PHASE_2};
+
+static enum CalibState calibration_state = POSITIONING;
+static bool calibration_in_progress = false;
+
 static thread_t *distThd;
+static uint16_t dist_mm = 0;
+static bool VL53L0X_configured = false;
+
 static THD_WORKING_AREA(waposition_calibrator, 512);
 static THD_FUNCTION(position_calibrator, arg) {
 
 	 // start of the calibration function
 
 	 //declaration of variables
-	    int last_distance=1;
-	    int new_distance=0;
-	    int limit=0;
-	    int save_measure1=0;
-	    int save_measure2=0;
-	    int first_measure=0; //gives the first measurment of distance
-	    int second_measure=0;// gives the seconf measurment after rotation
-	    int rotationclock=0; //0 is turning clockwise
-	    right_motor_set_speed(200);//the robot starts rotating
-	    left_motor_set_speed(-200);
-	    rotationclock=1;
 
-	 //init of the functions to start the TOF
+	float avg_dist_mm = 0.0;
+	float minimal_distance = MINIMAL_DIST;
+	int16_t min_x = 0;
+	int16_t min_y = 0;
 
 	chRegSetThreadName("VL53L0x Thd");
 	VL53L0X_Error status = VL53L0X_ERROR_NONE;
@@ -47,7 +58,7 @@ static THD_FUNCTION(position_calibrator, arg) {
 	status = VL53L0X_init(&device);
 
 	if(status == VL53L0X_ERROR_NONE){
-		VL53L0X_configAccuracy(&device, VL53L0X_LONG_RANGE);
+		VL53L0X_configAccuracy(&device, VL53L0X_HIGH_ACCURACY); //the TOF is set in Accuracy mode
 	}
 	if(status == VL53L0X_ERROR_NONE){
 		VL53L0X_startMeasure(&device, VL53L0X_DEVICEMODE_CONTINUOUS_RANGING);
@@ -62,70 +73,58 @@ static THD_FUNCTION(position_calibrator, arg) {
     		VL53L0X_getLastMeasure(&device);
    			dist_mm = device.Data.LastRangeMeasure.RangeMilliMeter;
     	}
-    	chprintf((BaseSequentialStream *)&SDU1, "testtest=%dus\n", 0);
-
-		chThdSleepMilliseconds(100);
-
-		//aditionnal code to do the calibration routine
-//calculation of the new measure using a sort of a filter
-
-		new_distance= dist_mm;
-    	/*chprintf((BaseSequentialStream *)&SDU1, "new_dist:=%dus\n", new_distance);
-    	chprintf((BaseSequentialStream *)&SDU1, "limit:=%dus\n", limit);
-    	chprintf((BaseSequentialStream *)&SDU1, "first_measure:=%dus\n", first_measure);
-    	chprintf((BaseSequentialStream *)&SDU1, "first_measure:=%dus\n", second_measure);*/
-
-
-
-
-			if(((last_distance+15)<new_distance) & (limit!=4)& (limit!=8)){
-			    		invers_speed(rotationclock);
-			    		limit+=1;
-			    		//chprintf((BaseSequentialStream *)&SDU1, "invers:=%dus\n", 0);
-			    		if (rotationclock==1){
-			    			rotationclock=0;
-			    		}
-			    		else{rotationclock=1;}
-
-				if(limit==3){
-					save_measure1=dist_mm;
+    	avg_dist_mm = avg_dist_mm*(1-AVG_MOV_K) + AVG_MOV_K*dist_mm;
+    	if(calibration_in_progress){
+			switch(calibration_state) {
+			case POSITIONING:{
+				if(get_position_achieved()){
+					set_angle_obj(CALIBRATION_ANGLE);
+					calibration_state=TURNING_TO_WALL_Y;
 				}
-				if(limit==4){
-					first_measure=(save_measure1+dist_mm)/2,
-					//chprintf((BaseSequentialStream *)&SDU1, "YES:=%dus\n", 0);
+				break;
+			}
+			case TURNING_TO_WALL_Y: {
+				if(get_direction_achieved()){
+					right_motor_set_speed(CALIBRATION_SPEED);//the robot starts rotating
+					left_motor_set_speed(-CALIBRATION_SPEED);
+					minimal_distance = MINIMAL_DIST;
+					calibration_state=CALIBRATING_PHASE_1;
 				}
-				if(limit==7){
-					save_measure2=dist_mm;
+				break;
+			}
+			case CALIBRATING_PHASE_1:{
+				if(avg_dist_mm< minimal_distance){
+					minimal_distance=avg_dist_mm;
+				}else if(avg_dist_mm > (minimal_distance)){
+					min_x = (int16_t)minimal_distance;
+					set_angle_calib(WALL_Y_ANGLE);
+					set_angle_obj(WALL_X_ANGLE);
+					calibration_state=TURNING_TO_WALL_X;
 				}
-				if(limit==8){
-				second_measure=(save_measure2+dist_mm)/2,
-				//chprintf((BaseSequentialStream *)&SDU1, "YES2:=%dus\n", 0);
+				break;
+			}
+			case TURNING_TO_WALL_X: {
+				if(get_direction_achieved()){
+					minimal_distance = MINIMAL_DIST;
+					calibration_state=CALIBRATING_PHASE_2;
 				}
-
-
+				break;
 			}
-			if((limit==4) & (part<25)){
-				part+=1;
-		    	//chprintf((BaseSequentialStream *)&SDU1, "part:=%dus\n", part);
-
+			case CALIBRATING_PHASE_2: {
+				if(avg_dist_mm< minimal_distance){
+					minimal_distance=avg_dist_mm;
+				}else if(avg_dist_mm > minimal_distance){
+					min_y = (int16_t)minimal_distance;
+					set_position_calib(min_x, min_y);
+					calibration_in_progress = false;
+				}
+				break;
 			}
-			if(part>=25){
-				part=0;
-				limit=5;
 			}
-			if(limit==8){
-			part=0;
-		    right_motor_set_speed(0);//the robot starts rotating
-		    left_motor_set_speed(0);
-			}
-		if (last_distance>new_distance){
-			last_distance=new_distance;
-		}
-		else{
-			if((last_distance+15)<new_distance){
-				last_distance=new_distance;
-			}
-		}
+			chThdSleepMilliseconds(THREAD_SLEEP_YES);
+    	}else{
+    		chThdSleepMilliseconds(THREAD_SLEEP_NO);
+    	}
     }
 }
 
@@ -143,13 +142,9 @@ void position_calibrator_start(void){
                      NULL);
 }
 
-void invers_speed(int sens){
-	if(sens==0){
-		right_motor_set_speed(200);
-		left_motor_set_speed(-200);
-	}
-	else{
-		right_motor_set_speed(-200);
-		left_motor_set_speed(200);
-	}
+void calibrate(void){
+	int16_t calibration_point[2] = {CORNER_DISTANCE, CORNER_DISTANCE};
+	set_position_obj(calibration_point);
+	calibration_in_progress = true;
+	calibration_state = POSITIONING;
 }
